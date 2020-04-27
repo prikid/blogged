a013e019ec8c850307834da8e79d1a1c077763dd
Serhii Riabcheniuk <sergey@prikid.ua>
2020-04-27 21:23:16 +0300

    remove asset_path url replacement for images in articles

--- src/Traits/HasMarkdownParser.php
+++ src/Traits/HasMarkdownParser.php
@@ -2,7 +2,6 @@
 
 namespace BinaryTorch\Blogged\Traits;
 
-use Illuminate\Support\Str;
 use ParsedownExtra;
 
 trait HasMarkdownParser
@@ -14,18 +13,6 @@ trait HasMarkdownParser
 	 */
 	public function parse($text)
 	{
-		//        return (new ParsedownExtra)->text($text);
-		$text = (new ParsedownExtra)->text($text);
-		if (function_exists('asset_path')) {
-			$text = preg_replace_callback(
-				"/(<img.+?src=[\"'])(.+?)([\"'].+?>)/",
-				function ($m) {
-					$s = Str::startsWith($m[2], 'http') ? $m[2] : asset_path($m[2]);
-					return $m[1] . $s . $m[3];
-				},
-				$text
-			);
-		}
-		return $text;
+		return (new ParsedownExtra)->text($text);
 	}
 }
